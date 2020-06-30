e60a52fe621a81e9a78ad53241607fafda8449a0
sergiu moraru <C:\Users\sergi\AppData\Roaming\The Bat!>
2020-06-30 12:06:46 +0300

    refactor for dynamic provider loading

--- index.js
+++ index.js
@@ -1,15 +1,97 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
-function loadAut() {
-  var options = {
-    client_id: "173fe996-3a74-4d81-928c-7ac9d6f44a",
-    client_secret: "your_client_secret",
-    scopes: ["Mail.ReadWrite", "offline_access", "Contacts.ReadWrite"], // Scopes limit access for OAuth tokens.
-  };
+const fetch = require("node-fetch");
 
+let win, authWindow;
+var options = {
+  // client_id: "173fe996-3a74-4d81-928c-7ac9d6f4994a",
+  // client_secret: "your_client_secret",
+  scopes: ["Mail.ReadWrite","openId", "profile", "Mail.ReadWrite", "offline_access", "Contacts.ReadWrite"], // Scopes limit access for OAuth tokens.
+  redirect_uri: 'https://login.microsoftonline.com/common/oauth2/nativeclient',
+};
+var postOpts = {
+  // client_id: "173fe996-3a74-4d81-928c-7ac9d6f4994a",
+  // client_secret: "your_client_secret",
+  // scopes: ["Mail.ReadWrite", "offline_access", "Contacts.ReadWrite"], // Scopes limit access for OAuth tokens.
+  redirect_uri: 'https://login.microsoftonline.com/common/oauth2/nativeclient',
+  grant_type: 'authorization_code'
+};
+
+function requestGithubToken(options, code) {
+  var oo = {...postOpts, code}
+
+  var formBody = [];
+  for (var property in oo) {
+    var encodedKey = encodeURIComponent(property);
+    var encodedValue = encodeURIComponent(oo[property]);
+    formBody.push(encodedKey + "=" + encodedValue);
+}
+  console.log('---oo', formBody)
+  fetch('https://login.microsoftonline.com/common/oauth2/v2.0/token', {
+    qs: options,
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    },
+    body: new URLSearchParams({
+      'userName': 'test@gmail.com',
+      'password': 'Password!',
+      'grant_type': 'authorization_code',
+      client_id: "173fe996-3a74-4d81-928c-7ac9d6f4994a",
+      code
+    }),
+  }).then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    console.log('res', data)
+    win.webContents.send('login', data.access_token);
+  })
+  // apiRequests
+  //   .post('https://github.com/login/oauth/access_token', {
+  //     client_id: options.client_id,
+  //     client_secret: options.client_secret,
+  //     code: code,
+  //   })
+  //   .end(function (err, response) {
+  //     if (response && response.ok) {
+  //     // Success - Received Token.
+  //     // Store it in localStorage maybe?
+  //     window.localStorage.setItem('githubtoken', response.body.access_token);
+  //     } else {
+  //     // Error - Show messages.
+  //     console.log(err);
+  //     }
+  //   });
+}
+function handleCallback(url) {
+  var raw_code = /code=.*?(&|$)/.exec(url) || null;
+  var code = raw_code && raw_code.length > 1 ? raw_code[0].slice(5) : null;
+  
+  var error = /\?error=(.+)\$/.exec(url);
+  console.log('=====', code)
+
+  if (code || error) {
+    // Close the browser if code found or error
+    authWindow.destroy();
+  }
+
+  // If there is a code, proceed to get token from github
+  if (code) {
+    requestGithubToken(options, code);
+  } else if (error) {
+    alert(
+      "Oops! Something went wrong and we couldn't" +
+        'log you in using Github. Please try again.'
+    );
+  }
+}
+
+function loadAut() {
+  
   // Build the OAuth consent page URL
-  var authWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  authWindow = new BrowserWindow({
+    width: 1200,
+    height: 900,
     show: false,
     "node-integration": false,
   });
@@ -17,15 +99,20 @@ function loadAut() {
   // var authUrl =
   //   githubUrl + 'client_id=' + options.client_id + '&response_type=code' +'&scope=' + options.scopes + '&redirect_uri=https://login.microsoftonline.com/common/oauth2/nativeclient';
   var authUrl = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=173fe996-3a74-4d81-928c-7ac9d6f4994a&scope=Mail.ReadWrite&response_type=code&redirect_uri=https://login.microsoftonline.com/common/oauth2/nativeclient";
-  authWindow.webContents.on(
-    "did-get-redirect-request",
-    function(event, oldUrl, newUrl) {
-      // handleCallback(newUrl);
-      console.log("=====", oldUrl, newUrl);
-    }
-  );
-  authWindow.webContents.on("will-navigate", function(event, url) {
-    console.log("=====", url);
+  // authWindow.webContents.on(
+  //   "did-get-redirect-request",
+  //   function(event, oldUrl, newUrl) {
+  //     // handleCallback(newUrl);
+  //     console.log("=====", oldUrl, newUrl);
+  //   }
+  // );
+  authWindow.once('did-finish-load', () => {
+    console.log('wwwwwww')
+    // Send Message
+ })
+  authWindow.webContents.on("did-navigate", function(event, url) {
+    // console.log("==will-navigate===", url);
+    handleCallback(url);
     // handleCallback(url);
   });
   authWindow.loadURL(authUrl);
@@ -33,9 +120,9 @@ function loadAut() {
 }
 function createWindow() {
   // Create the browser window.
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
+   win = new BrowserWindow({
+    width: 1200,
+    height: 900,
     webPreferences: {
       nodeIntegration: true,
     },
